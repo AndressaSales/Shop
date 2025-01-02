8595db1d97b427d6269965f950c6f3fe220c5c3a
AndressaSales <andressaccs2001@gmail.com>
2025-01-02 14:41:03 -0300

    finalizando a page kids

--- src/Pages/Kids.jsx
+++ src/Pages/Kids.jsx
@@ -1,5 +1,29 @@
+import bannerKids from "../assets/bannerKids.png"
+import {Shopcontext} from "../Context/ShopContext"
+import Item from "../Components/Item"
+import { useContext } from "react"
+
 export default function Kids(){
+
+    const {all_product} = useContext(Shopcontext)
+
+    const kidsProduct = all_product.filter((product) => product.category === 'kid')
+
     return(
-        <>Kids</>
+       <div className="md:mt-32 mt-20 max-w-7xl mx-auto">
+            <div>
+                <img src={bannerKids} className="w-screen px-6" />
+            </div>
+            <div className="mx-auto max-w-7xl px-4 py-16 sm:pt-24 lg:max-w-7xl lg:px-8">
+                <h2 className="text-2xl font-bold tracking-tight text-gray-900 text-center md:text-start">Coleção Infantil</h2>
+                <div className="mt-6 grid grid-col-1 gap-x-6 gap-y-10 px-6 md:px-0 sm:grid-cols-2 lg:grid-col-4 xl:gap-x-8">
+                    {
+                        kidsProduct.map((product) => {
+                            return <Item key={product.id} product={product} />
+                        })
+                    }
+                </div>
+            </div>
+       </div>
     )
 }
